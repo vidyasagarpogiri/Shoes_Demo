afe82c95207cb43abf45d5dd3b765a02e06ea51f
unknown <vidya.pogiri@kinnser.com>
2015-12-28 15:57:53 +0530

    Added Shies demo script

---
+++ Shoes_Demo.rb
@@ -0,0 +1,15 @@
+# sample program for Shoes
+
+require 'green_shoes'
+
+Shoes.app do
+  para "Hello World"
+end
+
+Shoes.app do
+   self.stack do
+     self.para "First"
+     self.para "Second"
+     self.para "Third"
+   end
+ end
