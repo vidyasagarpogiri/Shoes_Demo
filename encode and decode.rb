f44fd6532f0b33b0c16d5ba7e0b909eccb9fcea5
unknown <vidya.pogiri@kinnser.com>
2016-03-07 14:19:19 +0530

    added encode & decode

---
+++ encode and decode.rb
@@ -0,0 +1,6 @@
+irb(main):005:0> require "base64"
+=> true
+irb(main):006:0> Base64.encode64('Send reinforcements')
+=> "U2VuZCByZWluZm9yY2VtZW50cw==\n"
+irb(main):007:0> Base64.decode64('U2VuZCByZWluZm9yY2VtZW50cw==\n')
+=> "Send reinforcements"
