4a142ccd6bd1a5b6d686e51bee270ce758395a4b
unknown <vidya.pogiri@kinnser.com>
2016-03-23 13:24:27 +0530

    modified encode & decode

---
+++ encode_decode.rb
@@ -0,0 +1,8 @@
+#encode & decode of string in ruby language
+
+irb(main):005:0> require "base64"
+=> true
+irb(main):006:0> Base64.encode64('Send reinforcements')
+=> "U2VuZCByZWluZm9yY2VtZW50cw==\n"
+irb(main):007:0> Base64.decode64('U2VuZCByZWluZm9yY2VtZW50cw==\n')
+=> "Send reinforcements"
