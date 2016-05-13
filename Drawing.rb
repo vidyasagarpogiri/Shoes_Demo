594ce89ecaea78eb6cc9557c41c616a0dc9d99c1
unknown <vidya.pogiri@kinnser.com>
2016-05-13 14:15:58 +0530

    modified drawing file

--- Drawing.rb
+++ Drawing.rb
@@ -1,5 +1,5 @@
 # drawing.rb
-# Show how to draw basic shapes in Shoes example program for desktop application
+# Show how to draw basic shapes in Shoes example program for desktop app
 require 'green_shoes'
 
 Shoes.app do
